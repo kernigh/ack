kernigh/ack

commit 5bae29a00c4ca6f6dbd3d8dafde3e4eeecb131a3

ego now builds and is used.
This needed lots of refactoring to ego --- not all platforms have ego descr
files, and ego will just crash if you invoke it without one. I think originally
it was never intended that these platforms would be used at -O2 or above.

Plats now only specify the ego descr file if they have one.

util/ego/share/go.c
@@ -10,24 +10,24 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
 #include "types.h"
 #include "debug.h"
 #include "global.h"
-#include "files.h"
 #include "get.h"
 #include "put.h"
 #include "lset.h"
 #include "map.h"
 #include "alloc.h"
 #include "go.h"
+#include "files.h"
 
 STATIC bool report_flag = FALSE; /* report #optimizations found? */
 #ifdef DEBUG
 STATIC bool core_flag = FALSE; /* report core usage? */
 #endif
 
-STATIC mach_init(machfile, phase_machinit) char* machfile;
-int (*phase_machinit)();
+static mach_init(char* machfile, int (*phase_machinit)())
 {
 	/* Read target machine dependent information */
 
@@ -42,13 +42,10 @@ int (*phase_machinit)();
 	fclose(f);
 }
 
-go(argc, argv, initialize, optimize, phase_machinit, proc_flag) int argc;
-char* argv[];
-int (*initialize)();
-int (*optimize)();
-int (*phase_machinit)();
-int (*proc_flag)();
+void go(int argc, const char** argv,
+	int (*initialize)(), int (*optimize)(), int (*phase_machinit)(), int (*proc_flag)())
 {
+	struct files* files = findfiles(argc, argv);
 	FILE* f, *gf, *f2, *gf2; /* The EM input and output and
 				 * the basic block graphs input and output
 				 */
@@ -60,49 +57,56 @@ int (*proc_flag)();
 	bool time_opt = TRUE;
 
 	linecount = 0;
-	for (i = ARGSTART; i < argc; i++)
+	opterr = 0;
+	for (;;)
 	{
-		p = argv[i];
-		if (*p++ != '-')
-			error("illegal argument");
-		switch (*p)
+		int opt = getopt(files->argc, files->argv, "STM:CQV");
+		if (opt == -1)
+			break;
+
+		switch (opt)
 		{
 			case 'S':
 				time_opt = FALSE;
 				break;
+
 			case 'T':
 				time_opt = TRUE;
 				break;
+
 			case 'M':
-				p++;
-				mach_init(p, phase_machinit);
+				mach_init(optarg, phase_machinit);
 				break;
+
 			case 'C':
 #ifdef DEBUG
 				core_flag = TRUE;
 #endif
 				break;
+
 			case 'Q':
 				report_flag = TRUE;
 				break;
+
 			case 'V':
 				verbose_flag = TRUE;
 				break;
-			default:
-				(*proc_flag)(p);
+
+			case '?':
+				proc_flag(argv[optind - 1]);
 				break;
 		}
 	}
 	time_space_ratio = (time_opt ? 100 : 0);
-	fproc = getptable(pname); /* proc table */
-	fdblock = getdtable(dname); /* data block table */
+	fproc = getptable(files->pname_in); /* proc table */
+	fdblock = getdtable(files->dname_in); /* data block table */
 	(*initialize)();
 	if (optimize == no_action)
 		return;
-	f = openfile(lname, "r");
-	gf = openfile(bname, "r");
-	f2 = openfile(lname2, "w");
-	gf2 = openfile(bname2, "w");
+	f = openfile(files->lname_in, "r");
+	gf = openfile(files->bname_in, "r");
+	f2 = openfile(files->lname_out, "w");
+	gf2 = openfile(files->bname_out, "w");
 	mesregs = Lempty_set();
 	while (getunit(gf, f, &kind, &g, &l, &curproc, TRUE))
 	{
@@ -130,18 +134,18 @@ int (*proc_flag)();
 	fclose(f2);
 	fclose(gf);
 	fclose(gf2);
-	f = openfile(dname2, "w");
+	f = openfile(files->dname_out, "w");
 	putdtable(fdblock, f);
 	/* fclose(f); done by putdtable */
-	f = openfile(pname2, "w");
+	f = openfile(files->pname_out, "w");
 	putptable(fproc, f, TRUE);
 	/* fclose(f); done by putptable */
 	core_usage();
 }
 
-no_action() {}
+int no_action() {}
 
-core_usage()
+void core_usage(void)
 {
 #ifdef DEBUG
 	if (core_flag)
@@ -151,8 +155,7 @@ core_usage()
 #endif
 }
 
-report(s, n) char* s;
-int n;
+void report(char* s, int n)
 {
 	/* Report number of optimizations found, if report_flag is set */
 

build.lua
@@ -2,7 +2,7 @@ vars.cflags = {
 	"-g", "-O"
 }
 vars.ackcflags = {
-	"-O"
+	"-O6"
 }
 vars.plats = {
 	"cpm",
@@ -27,6 +27,7 @@ installable {
 		"util/ack+pkg",
 		"util/amisc+pkg",
 		"util/arch+pkg",
+		"util/ego+pkg",
 		"util/led+pkg",
 		"util/misc+pkg",
 		"util/opt+pkg",

lib/descr/fe
@@ -206,19 +206,6 @@ name ego
 	to .gk
 	program {EM}/lib/ack/em_ego
 	mapflag -EGO-* EGO_F={EGO_F?} -*
-# The following lines are obsolete, but here for backwards compatibility.
-# They should be removed some day.
-	mapflag -IL* EGO_F={EGO_F?} -IL*
-	mapflag -CS* EGO_F={EGO_F?} -CS*
-	mapflag -SR* EGO_F={EGO_F?} -SR*
-	mapflag -UD* EGO_F={EGO_F?} -UD*
-	mapflag -LV* EGO_F={EGO_F?} -LV*
-	mapflag -SA* EGO_F={EGO_F?} -RA*
-	mapflag -SP* EGO_F={EGO_F?} -SP*
-	mapflag -BO* EGO_F={EGO_F?} -BO*
-	mapflag -CJ* EGO_F={EGO_F?} -CJ*
-	mapflag -EV EGO_F={EGO_F?} -V
-# End of obsolete lines
 	mapflag -Q* EGO_F={EGO_F?} -Q*
 	mapflag -T* EGO_F={EGO_F?} -T*
 	mapflag -S* EGO_F={EGO_F?} -S*
@@ -227,7 +214,7 @@ name ego
 	mapflag -O* EGO_F={EGO_F?} -O*
 	args \
 		{EGO_F?} -P {EM}/lib/ack/ego \
-		-M{EM}/share/ack/ego/{ARCH}.descr <
+		{EGO_PLAT_FLAGS?} <
 	optimizer 2
 	stdout
 	combiner

plat/build.lua
@@ -20,6 +20,7 @@ definerule("ackfile",
 				"lang/pc/comp+pkg",
 				"plat/"..plat.."+tools",
 				"util/ack+pkg",
+				"util/ego+pkg",
 				"util/misc+pkg",
 				e.deps
 			},
@@ -67,7 +68,6 @@ definerule("ackprogram",
 				"plat/"..e.vars.plat.."+pkg",
 				"util/ack+pkg",
 				"util/led+pkg",
-				"util/amisc+pkg",
 				e.deps
 			},
 			_clibrary = acklibrary,

plat/linux386/descr
@@ -26,6 +26,7 @@ var C_LIB={PLATFORMDIR}/libc-ansi.a
 var CC_ALIGN=-Vr
 var OLD_C_LIB={C_LIB}
 var MACHOPT_F=-m10
+var EGO_PLAT_FLAGS=-M{EM}/share/ack/ego/{ARCH}.descr
 
 # Override the setting in fe so that files compiled for linux386 can see
 # the platform-specific headers.

plat/linux68k/descr
@@ -26,6 +26,7 @@ var C_LIB={PLATFORMDIR}/libc-ansi.a
 var CC_ALIGN=-Vr
 var OLD_C_LIB={C_LIB}
 var MACHOPT_F=-ml10
+var EGO_PLAT_FLAGS=-M{EM}/share/ack/ego/{ARCH}.descr
 
 # Override the setting in fe so that files compiled for linux68k can see
 # the platform-specific headers.

plat/pc86/descr
@@ -22,6 +22,7 @@ var PLATFORMDIR={EM}/share/ack/{PLATFORM}
 var CPP_F=-D__unix
 var ALIGN=-a0:1 -a1:1 -a2:1 -a3:1
 var MACHOPT_F=-m8
+var EGO_PLAT_FLAGS=-M{EM}/share/ack/ego/{ARCH}.descr
 
 # Override the setting in fe so that files compiled for this platform can see
 # the platform-specific headers.

util/ego/build.lua
@@ -1,0 +1,58 @@
+local function build_ego(name)
+	cprogram {
+		name = name,
+		srcs = { "./"..name.."/*.c" },
+		deps = {
+			"util/ego/share+lib",
+			"modules/src/em_data+lib",
+			"h+emheaders",
+		},
+		vars = {
+			["+cflags"] = {"-DVERBOSE", "-DNOTCOMPACT"}
+		}
+	}
+end
+
+build_ego("bo")
+build_ego("ca")
+build_ego("cf")
+build_ego("cj")
+build_ego("cs")
+build_ego("ic")
+build_ego("il")
+build_ego("lv")
+build_ego("sp")
+build_ego("sr")
+build_ego("ud")
+
+cprogram {
+	name = "em_ego",
+	srcs = { "./em_ego/em_ego.c" },
+	deps = {
+		"modules/src/print+lib",
+		"modules/src/string+lib",
+		"modules/src/system+lib",
+		"modules+headers",
+		"h+emheaders",
+	}
+}
+
+installable {
+	name = "pkg",
+	map = {
+		["$(PLATDEP)/em_ego"] = "+em_ego",
+		["$(PLATDEP)/ego/bo"] = "+bo",
+		["$(PLATDEP)/ego/ca"] = "+ca",
+		["$(PLATDEP)/ego/cf"] = "+cf",
+		["$(PLATDEP)/ego/cj"] = "+cj",
+		["$(PLATDEP)/ego/cs"] = "+cs",
+		["$(PLATDEP)/ego/ic"] = "+ic",
+		["$(PLATDEP)/ego/il"] = "+il",
+		["$(PLATDEP)/ego/lv"] = "+lv",
+		["$(PLATDEP)/ego/ra"] = "./ra+ra",
+		["$(PLATDEP)/ego/sp"] = "+sp",
+		["$(PLATDEP)/ego/sr"] = "+sr",
+		["$(PLATDEP)/ego/ud"] = "+ud",
+		"./descr+pkg",
+	}
+}

commit 915cd0dde1f137819f463e52d698b84f679759e2

Fix passing command-line flag from ego to phase
When the option is "-t", the phase's proc_flag expects a pointer to
(char)'t'.  This was broken since commit 5bae29a (in 2016) switched to
getopt().  Fix now, because I will edit the IL phase where -t keeps
temporary files.

util/ego/share/go.c
@@ -53,8 +53,6 @@ void go(int argc, char * const *argv,
 	bblock_p g;
 	line_p l;
 	short kind;
-	int i;
-	char* p;
 	bool time_opt = TRUE;
 
 	linecount = 0;
@@ -93,9 +91,11 @@ void go(int argc, char * const *argv,
 				verbose_flag = TRUE;
 				break;
 
-			case '?':
-				proc_flag(argv[optind - 1]);
+			case '?': {
+				char c = optopt;
+				(*proc_flag)(&c);
 				break;
+			}
 		}
 	}
 	time_space_ratio = (time_opt ? 100 : 0);